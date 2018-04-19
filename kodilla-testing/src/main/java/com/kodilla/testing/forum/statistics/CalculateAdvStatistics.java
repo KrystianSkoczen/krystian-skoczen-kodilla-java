ba15b850afaf3c74e9459d9c25ae1f0cd890529c
KrystianSkoczen <k.skoczen@wp.pl>
2018-04-19 10:49:27 +0200

    Bez out

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateAdvStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateAdvStatistics.java
@@ -1,8 +1,22 @@
 package com.kodilla.testing.forum.statistics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CalculateAdvStatistics {
 
-    public void calculateAdvStatistics(Statistics statistics) {
+    public HashMap<String, Integer> calculateAdvStatistics(Statistics statistics) {
+        public HashMap<String, Integer> numberUser(){
+            HashMap<String, Integer> resultUser = new HashMap<String, Integer>();
+            for(Map.Entry<String, Integer> user : users.getUsers().entrySet()) {
+                resultUser.size(user.getKey(), user.getValue());
+            }
+            return resultUser;
+        }
+    }
+
+    public void showStatistics(){
 
     }
+
 }
