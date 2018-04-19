f59c6d4c77dcfb0a1d56dccf0047eab3b50329c9
KrystianSkoczen <k.skoczen@wp.pl>
2018-04-19 21:54:07 +0200

    Module7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public void beautify(PoemDecorator poemDecorator) {
+        System.out.println("ABC Text ABC");
+    }
+    public void beautyfull(PoemDecorator poemDecorator) {
+        System.out.println("@@ Text @@");
+    }
+
+}
